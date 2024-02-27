3af126a6f6742585f54e5dba65e955beddaede56
numanijaz786 <nomiijaz6224@gmail.com>
2024-02-27 14:42:43 +0500

    bugfixing

--- helpers/email.js
+++ helpers/email.js
@@ -4,14 +4,14 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const Email = (msg) => {
   sgMail.send(msg).then(
     () => {
-      return 'Email sent successfully';
+      return true;
     },
     (error) => {
       console.error(error);
 
       if (error.response) {
         console.error(error.response.body);
-        return 'Something went wrong';
+        return false;
       }
     },
   );
