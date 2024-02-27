d82c302ba432c6ebcf3ce23cdf24a69f29fe6038
numanijaz786 <nomiijaz6224@gmail.com>
2024-02-27 17:55:45 +0500

    Resend verification code forgot resent password api

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,7 +21,7 @@ const userRegister = async (req, res) => {
         text: `Your confirmation code is ${code}`,
         html: `Welcome to our website, Your confirmation code is <strong>${code}</strong>`,
       };
-      const result =  Email(msg);
+      const result = Email(msg);
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -68,10 +68,39 @@ const confirmedCode = async (req, res) => {
   }
 };
 
+const resendCode = async (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({ status: 400, message: 'Email is required' });
+  }
+  const user = await User.findOne({ email });
+  if (user) {
+    const code = Math.floor(Math.random() * 9000) + 1000;
+    const msg = {
+      to: email,
+      from: process.env.ADMINEMAIL,
+      subject: 'Email Confirmation Code',
+      text: `Your confirmation code is ${code}`,
+      html: `Welcome to our website, Your confirmation code is <strong>${code}</strong>`,
+    };
+
+    const result = Email(msg);
+    user.confirmedCode = code;
+    await user.save();
+    return res.status(200).json({
+      status: 200,
+      message: 'Check your email for confirmation code',
+    });
+  }
+  return res.status(400).json({ status: 400, message: 'Email not found' });
+};
+
 const userLogin = async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-   return res.status(400).json({ status: 400, message: 'All fields are required' });
+    return res
+      .status(400)
+      .json({ status: 400, message: 'All fields are required' });
   }
   const user = await User.findOne({ email });
 
@@ -80,7 +109,7 @@ const userLogin = async (req, res) => {
       return res
         .status(400)
         .json({ status: 400, message: 'User Email is not confirmed' });
-    } 
+    }
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ status: 400, message: 'Wrong password' });
@@ -90,11 +119,74 @@ const userLogin = async (req, res) => {
     delete user._doc.password;
     res.status(200).json({ status: 200, token, user });
   } else {
-   return res.status(400).json({ status: 400, message: 'Email not found' });
+    return res.status(400).json({ status: 400, message: 'Email not found' });
   }
 };
+
+const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({ status: 400, message: 'Email is required' });
+  }
+  const user = await User.findOne({
+    email,
+  });
+  if (user) {
+    const resetUrlToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+    const resetUrl = `${process.env.CLIENT_URL}/reset-password/token=${resetUrlToken}`;
+    const msg = {
+      to: email,
+      from: process.env.ADMINEMAIL,
+      subject: 'Reset Password',
+      text: `Reset your password by clicking on this link ${resetUrl}`,
+      html: `Reset your password by clicking on this link <a href="${resetUrl}">${resetUrl}</a>`,
+    };
+    const result = Email(msg);
+    return res.status(200).json({
+      status: 200,
+      message: 'Check your email for reset link',
+    });
+  }
+  return res.status(400).json({ status: 400, message: 'Email not found' });
+};
+
+const resetPassword = async (req, res) => {
+  try {
+    const { password } = req.body;
+    const token = req.params.token;
+    if (!token || !password) {
+      return res
+        .status(400)
+        .json({ status: 400, message: 'All fields are required' });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id);
+
+    if (user) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      user.password = hashedPassword;
+      await user.save();
+      return res
+        .status(200)
+        .json({ status: 200, message: 'Password reset successfully' });
+    }
+    return res.status(400).json({ status: 400, message: 'Invalid token' });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ status: 500, message: 'Internal server error' });
+  }
+};
+
 module.exports = {
   userRegister,
   confirmedCode,
+  resendCode,
   userLogin,
+  forgotPassword,
+  resetPassword,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,10 +4,16 @@ const {
   userRegister,
   userLogin,
   confirmedCode,
+  forgotPassword,
+  resetPassword,
+  resendCode,
 } = require('../controllers/userController');
 
 router.post('/register', userRegister);
 router.post('/auth/confirmed', confirmedCode);
 router.post('/login', userLogin);
+router.post('/forgotpassword', forgotPassword);
+router.post('/resendcode', resendCode);
+router.post('/resetpassword/token=:token', resetPassword);
 
 module.exports = router;
