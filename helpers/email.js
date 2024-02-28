b9e16e8bf0b893d28f2bf89a171da1a031296b92
Ahmed Raza <ahmadrazach26@gmail.com>
2024-02-28 18:59:47 +0500

    Enhanced Email sending in helpers/email.js -  ensuring that the email sending process is awaited - any errors are caught and handled appropriately - preventing uncaught promise rejections and improving reliability

--- helpers/email.js
+++ helpers/email.js
@@ -1,20 +1,17 @@
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const Email = (msg) => {
-    sgMail.send(msg).then(
-        () => {
-            return true;
-        },
-        (error) => {
-            console.error(error);
-
-            if (error.response) {
-                console.error(error.response.body);
-                return false;
-            }
+const Email = async (msg) => {
+    try {
+        await sgMail.send(msg);
+        return true;
+    } catch (error) {
+        console.error(error);
+        if (error.response) {
+            console.error(error.response.body);
         }
-    );
+        return false;
+    }
 };
 
 module.exports = { Email };
--- models/userModel.js
+++ models/userModel.js
@@ -1,36 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  confirmedCode: {
-    type: Number,
-  },
-  isConfirmed: {
-    type: Boolean,
-    default: false,
-  },
-  profileImage: {
-    type: String,
-  },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    confirmedCode: {
+        type: Number
+    },
+    isConfirmed: {
+        type: Boolean,
+        default: false
+    },
+    profileImage: {
+        type: String
+    },
 
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
