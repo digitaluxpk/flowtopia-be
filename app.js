0598f50fa3c4d8a16b4ee386c7caa7930ab66fa8
numanijaz786 <nomiijaz6224@gmail.com>
2024-02-29 13:34:48 +0500

    add helmet as middleware

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const helmet = require("helmet");
 
 const cors = require("cors");
 const { connectToMongoDB } = require("./database/db");
 const userRoutes = require("./routes/userRoutes");
+
+// Middlewares
+app.use(helmet());
 app.use(
     cors({
         origin: "*",
@@ -13,13 +17,18 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Database connection
 connectToMongoDB();
 
 // Routes
+app.get("/", (req, res) => {
+    res.send("Welcome to the API");
+});
 app.use("/api/user", userRoutes);
 
+// Server
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
 });
