6c92e2c9ec8f234b44f2cbd323297561678371cd
numanijaz786 <nomiijaz6224@gmail.com>
2024-03-08 13:01:19 +0500

    code structured remove extra lines

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.get("/", (req, res) => {
 });
 app.use("/api/user", userRoutes);
 
+// Server
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
 });
