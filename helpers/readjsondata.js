baebb76b32e30a4adcce8c29aaedfa11fb1eaa4a
numanijaz786 <nomiijaz6224@gmail.com>
2024-03-08 13:06:23 +0500

    code structured remove extra lines

--- helpers/readjsondata.js
+++ helpers/readjsondata.js
@@ -3,16 +3,10 @@ const User = require("../models/userModel");
 
 const readJson = async (req, res) => {
     try {
-        console.log("Reading JSON file");
-
         const data = await fs.readFile("unified_customers.json", "utf8");
         const users = JSON.parse(data);
-
         for (const user of users) {
             const { name, email, description, created_at } = user;
-            console.log(name, email, description, created_at);
-            
-            // generate password for each user randomly
             const password = Math.random().toString(36).slice(-8);
 
             try {
@@ -25,14 +19,13 @@ const readJson = async (req, res) => {
                 });
                 await newUser.save();
             } catch (error) {
-                console.log(error);
+
                 return res.status(500).json({ error: error.message });
             }
         }
 
         res.json({ message: "Data uploaded successfully" });
     } catch (error) {
-        console.log(error);
         res.status(500).json({ error: error.message });
     }
 };
