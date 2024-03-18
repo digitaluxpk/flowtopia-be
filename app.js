5eb6bcb0c7ba532db34860f202eb470196902ca8
numanijaz786 <nomiijaz6224@gmail.com>
2024-03-18 14:27:25 +0500

    Socket implimantaion for Realtime Data api

--- app.js
+++ app.js
@@ -2,11 +2,24 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const helmet = require("helmet");
-
 const cors = require("cors");
 const { connectToMongoDB } = require("./database/db");
 const userRoutes = require("./routes/userRoutes");
+const { createServer } = require("http");
+const server = createServer(app);
+const socketIo = require("socket.io");
+const { emitRealTimeData, watchOptionsDataChanges } = require("./helpers/realTimeData");
+const io = socketIo(server);
 
+// Socket.IO connection event
+io.on("connection", (socket) => {
+    console.log("A user csonnected");
+    socket.on("getRealTimeData", async () => {
+        emitRealTimeData(socket);
+    });
+});
+// Start watching for changes in the MongoDB collection
+watchOptionsDataChanges(io);
 // Middlewares
 app.use(helmet());
 app.use(
@@ -23,12 +36,12 @@ connectToMongoDB();
 
 // Routes
 app.get("/", (req, res) => {
-    res.send("Welcome to the API");
+    res.json({ message: "Welcome to the Flowtopia API" });
 });
 app.use("/api/user", userRoutes);
 
 // Server
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running at port ${port}`);
 });
